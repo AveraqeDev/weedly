b0ea86b575b80213e6ba64ae54e3cc5aaf15cefc
Matt <matthew.wagaman@wpromote.com>
2022-07-16 21:03:03 -0700

    fix bug with dislikes page

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -5,6 +5,7 @@ import {
   ChevronUpIcon,
   DotsHorizontalIcon,
   MinusCircleIcon,
+  MinusIcon,
 } from "@heroicons/react/outline";
 import type { NextPage } from "next";
 import Head from "next/head";
@@ -44,117 +45,128 @@ const Favorites: NextPage = withPageAuthRequired(({ user }) => {
           </div>
           <div className="bg-white shadow sm:rounded-md">
             <ul className="divide-y divide-gray-200">
-              {userFavorites.map((favorite) => (
-                <li key={favorite.id}>
-                  <Disclosure>
-                    {({ open }) => (
-                      <>
-                        <div className="block">
-                          <div className="px-4 py-4 sm:px-6">
-                            <div className="flex items-center justify-between">
-                              <p className="flex items-center gap-2">
-                                <Disclosure as="div">
-                                  {({ open, close }) => (
-                                    <div className="relative">
-                                      <Disclosure.Button className="group rounded-full p-1 hover:bg-lime-500 hover:text-white focus:outline-none">
-                                        <DotsHorizontalIcon
-                                          className={classNames(
-                                            open ? "rotate-180 transform" : "",
-                                            "h-5 w-5 text-gray-500 group-hover:text-white"
-                                          )}
-                                        />
-                                      </Disclosure.Button>
-                                      <Disclosure.Panel className="origin-top-left absolute left-0 z-20">
-                                        <div className="flex flex-col bg-white py-1 ring-1 ring-gray-200 rounded-md">
-                                          <button
-                                            disabled={isDeleting}
-                                            className="disabled:cursor-not-allowed disabled:bg-gray-200 group flex items-center px-2 py-1 text-sm text-lime-600 hover:text-white hover:bg-lime-500"
-                                            onClick={() => {
-                                              deleteFavorite({
-                                                id: favorite.id,
-                                              });
-                                              close();
-                                            }}
-                                          >
-                                            {isDeleting ? (
-                                              <div className="pr-2">
-                                                <Spinner size={5} />
-                                              </div>
-                                            ) : (
-                                              <MinusCircleIcon
-                                                className="text-lime-600 group-hover:text-white mr-3 h-6 w-6"
-                                                aria-hidden="true"
-                                              />
+              {userFavorites.length > 0 ? (
+                userFavorites.map((favorite) => (
+                  <li key={favorite.id}>
+                    <Disclosure>
+                      {({ open }) => (
+                        <>
+                          <div className="block">
+                            <div className="px-4 py-4 sm:px-6">
+                              <div className="flex items-center justify-between">
+                                <p className="flex items-center gap-2">
+                                  <Disclosure as="div">
+                                    {({ open, close }) => (
+                                      <div className="relative">
+                                        <Disclosure.Button className="group rounded-full p-1 hover:bg-lime-500 hover:text-white focus:outline-none">
+                                          <DotsHorizontalIcon
+                                            className={classNames(
+                                              open
+                                                ? "rotate-180 transform"
+                                                : "",
+                                              "h-5 w-5 text-gray-500 group-hover:text-white"
                                             )}
-                                            <span>Delete</span>
-                                          </button>
-                                        </div>
-                                      </Disclosure.Panel>
-                                    </div>
-                                  )}
-                                </Disclosure>
-                                <span className="text-md font-medium text-lime-600">
-                                  {favorite.product.name}
-                                </span>
-                                <span className="text-sm text-gray-500">
-                                  ({favorite.product.brand})
-                                </span>
-                              </p>
-                              <div className="ml-2 flex-shrink-0 flex">
-                                <p
-                                  className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full`}
-                                >
-                                  ${favorite.product.price}
+                                          />
+                                        </Disclosure.Button>
+                                        <Disclosure.Panel className="origin-top-left absolute left-0 z-20">
+                                          <div className="flex flex-col bg-white py-1 ring-1 ring-gray-200 rounded-md">
+                                            <button
+                                              disabled={isDeleting}
+                                              className="disabled:cursor-not-allowed disabled:bg-gray-200 group flex items-center px-2 py-1 text-sm text-lime-600 hover:text-white hover:bg-lime-500"
+                                              onClick={() => {
+                                                deleteFavorite({
+                                                  id: favorite.id,
+                                                });
+                                                close();
+                                              }}
+                                            >
+                                              {isDeleting ? (
+                                                <div className="pr-2">
+                                                  <Spinner size={5} />
+                                                </div>
+                                              ) : (
+                                                <MinusCircleIcon
+                                                  className="text-lime-600 group-hover:text-white mr-3 h-6 w-6"
+                                                  aria-hidden="true"
+                                                />
+                                              )}
+                                              <span>Delete</span>
+                                            </button>
+                                          </div>
+                                        </Disclosure.Panel>
+                                      </div>
+                                    )}
+                                  </Disclosure>
+                                  <span className="text-md font-medium text-lime-600">
+                                    {favorite.product.name}
+                                  </span>
+                                  <span className="text-sm text-gray-500">
+                                    ({favorite.product.brand})
+                                  </span>
                                 </p>
-                              </div>
-                            </div>
-                            <div className="mt-2 sm:flex sm:justify-between">
-                              <div className="sm:flex sm:gap-1">
-                                <p className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-                                  {favorite.product.type}
-                                </p>
-                                {favorite.product.tags.map((tag) => (
+                                <div className="ml-2 flex-shrink-0 flex">
                                   <p
-                                    key={tag.id}
-                                    className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium tag-${tag.color} text-white`}
+                                    className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full`}
                                   >
-                                    {tag.name}
+                                    ${favorite.product.price}
                                   </p>
-                                ))}
+                                </div>
                               </div>
-                              <Disclosure.Button className="group rounded-full p-1 hover:bg-lime-500 hover:text-white focus:outline-none">
-                                <ChevronUpIcon
-                                  className={classNames(
-                                    open ? "rotate-180 transform" : "",
-                                    "h-5 w-5 text-gray-500 group-hover:text-white"
-                                  )}
-                                />
-                              </Disclosure.Button>
-                              <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                                <CalendarIcon
-                                  className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                                  aria-hidden="true"
-                                />
-                                <p>
-                                  Favorited on{" "}
-                                  <time dateTime={formatDate(favorite.addedAt)}>
-                                    {formatDate(favorite.addedAt)}
-                                  </time>
-                                </p>
+                              <div className="mt-2 sm:flex sm:justify-between">
+                                <div className="sm:flex sm:gap-1">
+                                  <p className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+                                    {favorite.product.type}
+                                  </p>
+                                  {favorite.product.tags.map((tag) => (
+                                    <p
+                                      key={tag.id}
+                                      className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium tag-${tag.color} text-white`}
+                                    >
+                                      {tag.name}
+                                    </p>
+                                  ))}
+                                </div>
+                                <Disclosure.Button className="group rounded-full p-1 hover:bg-lime-500 hover:text-white focus:outline-none">
+                                  <ChevronUpIcon
+                                    className={classNames(
+                                      open ? "rotate-180 transform" : "",
+                                      "h-5 w-5 text-gray-500 group-hover:text-white"
+                                    )}
+                                  />
+                                </Disclosure.Button>
+                                <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                                  <CalendarIcon
+                                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                    aria-hidden="true"
+                                  />
+                                  <p>
+                                    Favorited on{" "}
+                                    <time
+                                      dateTime={formatDate(favorite.addedAt)}
+                                    >
+                                      {formatDate(favorite.addedAt)}
+                                    </time>
+                                  </p>
+                                </div>
                               </div>
                             </div>
                           </div>
-                        </div>
-                        <Disclosure.Panel className="p-4">
-                          <p className="text-sm">
-                            {favorite.product.description}
-                          </p>
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
-                </li>
-              ))}
+                          <Disclosure.Panel className="p-4">
+                            <p className="text-sm">
+                              {favorite.product.description}
+                            </p>
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  </li>
+                ))
+              ) : (
+                <div className="flex items-center gap-2 p-4">
+                  <MinusIcon className="h-5 w-5 bg-red-500 rounded-full text-white" />
+                  <p className="text-gray-500 text-xs">No favorites found</p>
+                </div>
+              )}
             </ul>
           </div>
         </div>
--- src/pages/ratings.tsx
+++ src/pages/ratings.tsx
@@ -1,6 +1,10 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { Disclosure } from "@headlessui/react";
-import { CalendarIcon, ChevronUpIcon } from "@heroicons/react/outline";
+import {
+  CalendarIcon,
+  ChevronUpIcon,
+  MinusIcon,
+} from "@heroicons/react/outline";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Spinner from "../components/Spinner";
@@ -44,78 +48,87 @@ const Ratings: NextPage = withPageAuthRequired(({ user }) => {
           </div>
           <div className="bg-white shadow sm:rounded-md">
             <ul className="divide-y divide-gray-200">
-              {userRatings.map((rating) => (
-                <li key={rating.id}>
-                  <Disclosure>
-                    {({ open }) => (
-                      <>
-                        <div className="block">
-                          <div className="px-4 py-4 sm:px-6">
-                            <div className="flex items-center justify-between">
-                              <p className="flex items-center gap-2 truncate">
-                                <span className="text-md font-medium text-lime-600">
-                                  {rating.product.name}
-                                </span>
-                                <span className="text-sm text-gray-500">
-                                  ({rating.product.brand})
-                                </span>
-                              </p>
-                              <div className="ml-2 flex-shrink-0 flex">
-                                <p
-                                  className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full rating-${getColorFromRating(
-                                    rating.rating
-                                  )}`}
-                                >
-                                  {rating.rating} Star
-                                  {rating.rating > 1 && "s"}
+              {userRatings.length > 0 ? (
+                userRatings.map((rating) => (
+                  <li key={rating.id}>
+                    <Disclosure>
+                      {({ open }) => (
+                        <>
+                          <div className="block">
+                            <div className="px-4 py-4 sm:px-6">
+                              <div className="flex items-center justify-between">
+                                <p className="flex items-center gap-2 truncate">
+                                  <span className="text-md font-medium text-lime-600">
+                                    {rating.product.name}
+                                  </span>
+                                  <span className="text-sm text-gray-500">
+                                    ({rating.product.brand})
+                                  </span>
                                 </p>
-                              </div>
-                            </div>
-                            <div className="mt-2 sm:flex sm:justify-between">
-                              <div className="sm:flex sm:gap-1">
-                                <p className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-                                  {rating.product.type}
-                                </p>
-                                {rating.product.tags.map((tag) => (
+                                <div className="ml-2 flex-shrink-0 flex">
                                   <p
-                                    key={tag.id}
-                                    className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium tag-${tag.color} text-white`}
+                                    className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full rating-${getColorFromRating(
+                                      rating.rating
+                                    )}`}
                                   >
-                                    {tag.name}
+                                    {rating.rating} Star
+                                    {rating.rating > 1 && "s"}
                                   </p>
-                                ))}
+                                </div>
                               </div>
-                              <Disclosure.Button className="group rounded-full p-1 hover:bg-lime-500 hover:text-white focus:outline-none">
-                                <ChevronUpIcon
-                                  className={classNames(
-                                    open ? "rotate-180 transform" : "",
-                                    "h-5 w-5 text-gray-500 group-hover:text-white"
-                                  )}
-                                />
-                              </Disclosure.Button>
-                              <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                                <CalendarIcon
-                                  className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                                  aria-hidden="true"
-                                />
-                                <p>
-                                  Rated on{" "}
-                                  <time dateTime={formatDate(rating.createdAt)}>
-                                    {formatDate(rating.createdAt)}
-                                  </time>
-                                </p>
+                              <div className="mt-2 sm:flex sm:justify-between">
+                                <div className="sm:flex sm:gap-1">
+                                  <p className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+                                    {rating.product.type}
+                                  </p>
+                                  {rating.product.tags.map((tag) => (
+                                    <p
+                                      key={tag.id}
+                                      className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium tag-${tag.color} text-white`}
+                                    >
+                                      {tag.name}
+                                    </p>
+                                  ))}
+                                </div>
+                                <Disclosure.Button className="group rounded-full p-1 hover:bg-lime-500 hover:text-white focus:outline-none">
+                                  <ChevronUpIcon
+                                    className={classNames(
+                                      open ? "rotate-180 transform" : "",
+                                      "h-5 w-5 text-gray-500 group-hover:text-white"
+                                    )}
+                                  />
+                                </Disclosure.Button>
+                                <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                                  <CalendarIcon
+                                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                    aria-hidden="true"
+                                  />
+                                  <p>
+                                    Rated on{" "}
+                                    <time
+                                      dateTime={formatDate(rating.createdAt)}
+                                    >
+                                      {formatDate(rating.createdAt)}
+                                    </time>
+                                  </p>
+                                </div>
                               </div>
                             </div>
                           </div>
-                        </div>
-                        <Disclosure.Panel className="p-4">
-                          <p className="text-sm">{rating.review}</p>
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
-                </li>
-              ))}
+                          <Disclosure.Panel className="p-4">
+                            <p className="text-sm">{rating.review}</p>
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  </li>
+                ))
+              ) : (
+                <div className="flex items-center gap-2 p-4">
+                  <MinusIcon className="h-5 w-5 bg-red-500 rounded-full text-white" />
+                  <p className="text-gray-500 text-xs">No ratings found</p>
+                </div>
+              )}
             </ul>
           </div>
         </div>
